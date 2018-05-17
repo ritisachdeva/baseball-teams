5f418bc853d0ec4ea824fe28908497bb5f89e308
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-17 00:09:16 -0400

    Add CSS and get fav team names to appear in a list

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const form= document.querySelector('form#teamForm')
+const teamsArray = []
+
+const renderTeamItem = function(team) {
+    const item = document.createElement('li');
+    item.textContent = `Team: ${team}`;
+    return item;
+ }
+
+ const renderTeam = function(teamName) {
+    const list = document.createElement('ul');
+    list.appendChild(renderTeamItem(teamName));
+    return list;
+}
+
+// function teamsItem(team){
+//     const div = document.createElement('div')
+    
+
+//     const item = document.createElement('li')
+//     item.textContent = team
+//     //item.style.fontSize='20px'
+//     //item.style.textAlign='center'
 
 
+//     div.appendChild(item)
+
+//     return div
+// }
 
 const handleSubmit = function(ev){
     ev.preventDefault()
+    const f = ev.target
+    const teamForm = document.querySelector('#teamForm')
+    const favTeam = f.favTeam.value
 
-    const favTeam = form.favTeam.value
+    teamForm.appendChild(renderTeam(favTeam))
+    // const teamList = document.createElement('ul')
+    // teamList.appendChild(teamsItem(favTeam))
+    // teamForm.appendChild(teamList)
 
+    teamsArray.push(favTeam) 
     form.reset()
     form.favTeam.focus()
 }
